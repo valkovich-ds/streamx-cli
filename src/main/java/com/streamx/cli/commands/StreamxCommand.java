c2f9cffec5cd838c62cf029c286080d1b8df0f71
Kiryl Valkovich <kiryl.valkovich@ds.pl>
2026-09-14 07:48:11 +0200

    Demo: docs site and generated command reference on top of 2.0.4-rc.acb4ea5

--- src/main/java/com/streamx/cli/commands/StreamxCommand.java
+++ src/main/java/com/streamx/cli/commands/StreamxCommand.java
@@ -13,6 +13,7 @@ import com.streamx.cli.commands.completion.CompleteSettingsKeysCommand;
 import com.streamx.cli.commands.completion.CompleteSettingsSetKeysCommand;
 import com.streamx.cli.commands.completion.CompleteTemplateIdsCommand;
 import com.streamx.cli.commands.completion.CompletionCommand;
+import com.streamx.cli.commands.docs.GenerateDocsCommand;
 import com.streamx.cli.commands.info.InfoCommand;
 import com.streamx.cli.commands.local.LocalCommand;
 import com.streamx.cli.commands.org.OrgCommand;
@@ -46,7 +47,8 @@ import picocli.CommandLine;
         CompleteProjectIdsCommand.class,
         CompleteOrgMemberIdsCommand.class,
         CompleteInvitedEmailsCommand.class,
-        CompleteClusterIdsCommand.class
+        CompleteClusterIdsCommand.class,
+        GenerateDocsCommand.class
     }
 )
 public class StreamxCommand extends AbstractCommandGroup {
